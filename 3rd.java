dee5cfa9fefa3a49fb3ec223bd7cd11a594b2f97
Sajib Chandra Roy <sajibchandrar@gmail.com>
2023-10-02 21:09:51 +0600

    Modified the file and Added

--- 3rd.java
+++ 3rd.java
@@ -1,5 +1,27 @@
-class Third{
-    public static void main(String[] args) {
-        System.out.println("same as possible");
+import java.io.IOException;
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        /**
+         * Escreva a sua solução aqui
+         * Code your solution here
+         * Escriba su solución aquí
+         */
+         try{
+            System.out.println("Hello World!");
+         }
+         catch(Exception e){
+            System.out.println(e);
+         }
+ 
     }
+ 
 }
