f1d6f9ce9d643b1e4869c4782b812cf96f76d1f5
Sajib Chandra Roy <sajibchandrar@gmail.com>
2023-10-03 21:45:54 +0600

    Let check var & let degerene

--- Hello.js
+++ Hello.js
@@ -1,1 +1,23 @@
 console.log("Hello timtim");
+
+
+let a =10;
+console.log(a);
+
+a=15;
+console.log(a);
+
+a=13;
+
+console.log(a);
+
+
+var b=15;
+console.log(b);
+
+b=30;
+
+console.log(b);
+
+var b =14;
+console.log(b);
