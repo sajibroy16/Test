2611916deac947bc8c8dcaf0d51fe21b4f715305
Sajib Chandra Roy <sajibchandrar@gmail.com>
2023-10-06 11:10:01 +0600

    Again Modified by Timtim

--- Hello.js
+++ Hello.js
@@ -1,26 +1,92 @@
-console.log("Hello timtim");
+// console.log("Hello timtim");
 
 
-let a =10;
-console.log(a);
+// let a =10;
+// console.log(a);
 
-a=15;
-console.log(a);
+// a=15;
+// console.log(a);
 
-a=13;
+// a=13;
 
-console.log(a);
-
-
-var b=15;
-console.log(b);
-
-b=30;
-
-console.log(b);
-
-var b =14;
-console.log(b);
+// console.log(a);
 
 
-console.log(a+b);
+// var b=15;
+// console.log(b);
+
+// b=30;
+
+// console.log(b);
+
+// var b =14;
+// console.log(b);
+
+
+// console.log(a+b);
+
+// console.log(newFunction(4, 6));
+
+// function newFunction(a, b){
+//     return a+b;
+// }
+
+// function callSuccess(){
+//     console.log("Hi, timtim!");
+//     proposeSuccess();
+// }
+// function proposeSuccess(){
+//     console.log("Can you hear me? i Love you...");
+//     replyTimtim();
+// }
+
+// function replyTimtim(){
+//     console.log("NO! i hava a bf...");
+// }
+
+// // console.log(callSuccess());
+// // console.log(proposeSuccess());
+// // console.log(replyTimtim());
+
+// console.log(callSuccess());
+
+var constObject = [
+{
+    name: "Sajib",
+    status: "Student"
+},
+{
+    name: "Timtim",
+    status: "BSc Student"
+},
+{
+    name: "Roy"
+},
+{
+    name: "Raj"
+}
+
+];
+
+constObject.map((item, indx)=>{
+    console.log(item);
+});
+
+const prices = [1000, 80000, 350, 5368, 3344];
+
+var output = [];
+
+prices.forEach((item) =>{
+    // console.log(item*.5);
+    output.push(item*.5);
+}
+
+);
+
+// console.log(out)
+
+const newPrices = prices.map((item)=>{
+    return item*.5;
+});
+
+// console.log(newPrices);
--- SumTwoNubers.java
+++ SumTwoNubers.java
@@ -1,8 +1,117 @@
 class SumTwo{
+
+    public static void Recursion(int low, int up, int sum){
+
+        if(low==up){
+            sum+=low;
+            System.out.println(sum);
+            return;
+        }
+        // System.out.println(low);
+        sum+=low;
+        
+        Recursion(low+1, up, sum);
+
+    }
+    public static void SumRecursion(int n){
+        if(n==10){
+            return;
+        }
+        
+        System.out.println(n);
+
+        SumRecursion(n+1);
+    }
+
+    public static int fact(int n){
+        // int fac=1;
+        // for(int i=1; i<=n; i++){
+        //     fac *= i;
+        // }
+        // System.out.println(fac);
+            if(n==1 || n==0){
+                
+                return 1;
+            }
+            int cal_N = fact(n-1);
+            int calFact = n * cal_N;
+            return calFact;
+        
+    }
+
+
+    public static void reverseNumber(int N){
+        int reverse =0;
+        while(N!=0){
+            int temp = N%10;
+            reverse = reverse*10 + temp;
+            // int N1 = (reverse * 10);
+            N/=10;
+
+            
+        }
+        System.out.println(reverse);
+    }
+    public static void reverseString(String name)
+    {
+        String store = name;
+        
+        // while(name.length()!=0){
+            
+        // }
+
+        StringBuilder sb = new StringBuilder(name);
+
+        sb.reverse();
+
+        System.out.println(sb.toString());
+
+    }
+
+    public static void reverseStringRecursion(String name, int indx){
+        if(indx==0){
+            System.out.println(name.charAt(indx));
+            return;
+        }
+        System.out.print(name.charAt(indx));
+        reverseStringRecursion(name, indx-1);
+    }
+
+    public static void trackString(String name, char A){
+        
+    }
+
+    public static void Number(int number, int N)
+    {
+        int count=0;
+
+        while(number!=0)
+        {
+            int temp = number%10;
+            if(temp==N){
+                count++;
+            }
+            number/=10;
+        }
+        System.out.println(count);
+    }
     public static void main(String[] args) {
-        int a =10;
-        int b=39;
-        int sum = a+b;
-        System.out.println(sum);
+        // int a =10;
+        // int b=39;
+        // int sum = a+b;
+        // System.out.println(sum);
+
+        // Recursion(1, 10, 0);
+        
+        // System.out.println(fact(5));
+
+        Number(1222255566, 5);
+        // SumRecursion(1);
+
+        // reverseNumber(123);
+
+        // reverseString("HI Timtim");
+        // String name = "Hi timtim";
+        // reverseStringRecursion(name, name.length()-1);
     }
 }
